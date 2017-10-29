2f936ba1aa70eeab57a05513dd98e890855d0600
quangtung97 <quangtung29121997@gmail.com>
2017-10-29 08:30:42 +0700

    Add Asset

---
+++ include/graphics/gl/sprite_drawable.hpp
@@ -0,0 +1,17 @@
+#ifndef GRAPHICS_GL_SPRITE_DRAWABLE_HPP
+#define GRAPHICS_GL_SPRITE_DRAWABLE_HPP
+
+#include "drawable.hpp"
+
+namespace tung {
+
+class SpriteDrawable: public Drawable {
+private:
+
+public:
+
+};
+
+} // namespace tung
+
+#endif
---
+++ src/graphics/gl/sprite_drawable.cpp
@@ -0,0 +1,5 @@
+#include <graphics/gl/sprite_drawable.hpp>
+
+namespace tung {
+
+} // namespace tung
